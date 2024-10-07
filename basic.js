43a714164dc3628d780bd5e38b61a9b9a6e660a2
Ivan <ivanlopezasdf@gmail.com>
2024-10-07 12:25:20 +0200

    ejercicio 13

--- basic.js
+++ basic.js
@@ -140,7 +140,7 @@ console.log(result)
 
 //Ejercicio 13: completa el ejercicio
 
-let result2 = //Realiza la operación de comparación que consideres con las dos variables del ejercicio anterior para que el resultado en consola sea true.
+let result2 =  num3 == "1" //Realiza la operación de comparación que consideres con las dos variables del ejercicio anterior para que el resultado en consola sea true.
 console.log(result2)
 
 
